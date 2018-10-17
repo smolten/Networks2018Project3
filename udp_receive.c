7ca77c46743912560cbabb19bccbd2d5e0db0fb7
Scotty Molt <smolt@asu.edu>
2018-10-17 14:12:57 -0700

    More UDP cleanup

--- udp_receive.c
+++ udp_receive.c
@@ -11,8 +11,9 @@ int main() {
 
     //claim socket
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-	printf("socket() error: %s.\n", strerror(errno));
+    if (sockfd < 0)
+    {
+        printf("socket() error: %s.\n", strerror(errno));
         return -1;
     }
 
@@ -23,31 +24,28 @@ int main() {
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(32000);
 
-    //bind to socket
-    bind(sockfd, 
-         (struct sockaddr *) &servaddr, 
-         sizeof(servaddr));
-
-    //listen
+    //listen for messages
+    bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
     char recv_buffer[1024];
     struct sockaddr_in cliaddr;
     while (1) {
         socklen_t len = sizeof(cliaddr);
         int ret = recvfrom(sockfd, 
-                 recv_buffer, 
-                 sizeof(recv_buffer), 
-                 0,
-                 (struct sockaddr *) &cliaddr,
-                 &len);
+            recv_buffer, 
+            sizeof(recv_buffer), 
+            0,
+            (struct sockaddr *) &cliaddr,
+            &len);
 
         if (ret <= 0) {
             printf("recvfrom() error: %s.\n", strerror(errno));
             return -1;
         }
 
+        char* srcAddress = inet_ntoa(cliaddr.sin_addr);
         printf("[from %s] %s", 
-               inet_ntoa(cliaddr.sin_addr), 
-               recv_buffer);
+        	srcAddress, 
+        	recv_buffer);
     }
 
     return 0;
--- udp_send.c
+++ udp_send.c
@@ -9,43 +9,41 @@
 
 int main() {
 
-    //Claim socket
-    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) {
-	   printf("socket() error: %s.\n", strerror(errno));
-        return -1;
-    }
+	//Claim socket
+	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0)
+	{
+		printf("socket() error: %s.\n", strerror(errno));
+		return -1;
+	}
 
-    //make server address
-    struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr 
-        = inet_addr("127.0.0.1");
-    servaddr.sin_port = htons(32000);
+	//make server address
+	struct sockaddr_in servaddr;
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	servaddr.sin_port = htons(32000);
 
-    //Send messages
-    char send_buffer[1024]; 
-    while (1) {
+	//Send messages
+	char send_buffer[1024]; 
+	while (1)
+	{
+		fgets(send_buffer, sizeof(send_buffer), stdin);
 
-        fgets(send_buffer, 
-              sizeof(send_buffer), 
-              stdin);
+		int ret = sendto(sockfd, 
+			send_buffer, 
+			sizeof(send_buffer), 
+			0,
+			(struct sockaddr *) &servaddr,
+			sizeof(servaddr));
 
-        int ret = sendto(sockfd, 
-               send_buffer, 
-               sizeof(send_buffer), 
-               0,
-               (struct sockaddr *) &servaddr,
-               sizeof(servaddr));
+		if (ret <= 0)
+		{
+			printf("recvfrom() error: %s.\n", strerror(errno));
+			return -1;
+		}
 
-        if (ret <= 0) {
-            printf("recvfrom() error: %s.\n", strerror(errno));
-            return -1;
-        }
+	}
 
-    }
-
-    return 0;
+	return 0;
 }
-
